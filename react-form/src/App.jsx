ba4ff4ee96a0abc12eb51236960e0ff7241ced96
alexbiviano <bivianoalex63@gmail.com>
2025-05-09 15:31:18 +0200

    [upd] creazione progetto

---
+++ react-form/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+function App() {  
+  // Array di articoli (titoli)  
+  const articles = [  
+    "Pasta cacio e pepe",  
+    "Pasta alla carbonara",  
+    "Pasta all'amatriciana",  
+    "Pasta alla gricia",  
+    "Pasta alla puttanesca"  
+  ];  
+
+  return (  
+    <div className="App">  
+      <h1>Lista di Articoli</h1>  
+      <ul>  
+        {articles.map((article, index) => (  
+          <li key={index}>{article}</li>  
+        ))}  
+      </ul>  
+    </div>  
+  );  
+}  
+
+export default App;  
